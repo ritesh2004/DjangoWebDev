5116a528e4cf2b16ccb79250dce7682a5f5ce7c7
Ritesh Pramanik <109234507+ritesh2004@users.noreply.github.com>
2023-05-29 11:57:29 +0530

    updated

--- testapi/home/models.py
+++ testapi/home/models.py
@@ -5,5 +5,6 @@ class Books(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=500)
     author = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return self.title
